21e4398f170290905819936eee439dafa8c12829
Ambjörn Fagerström <ambjorn89@gmail.com>
2022-12-12 16:53:51 +0100

    Fix pusher configs

--- pusher.ts
+++ pusher.ts
@@ -1,16 +1,15 @@
 ﻿import Pusher from "pusher";
 import ClientPusher from "pusher-js";
 
-
 export const serverPusher = new Pusher({
-  appId: "1523696",
-  key: "2e3d1ce8d8cdfead1b2f",
-  secret: "5dd603b92522f06eca93",
+  appId: `${process.env.PUSHER_ID}`,
+  key: `${process.env.PUSHER_KEY}`,
+  secret: `${process.env.PUSHER_SECRET}`,
   cluster: "eu",
   useTLS: true,
 });
 
-export const clientPusher = new ClientPusher("2e3d1ce8d8cdfead1b2f", {
+export const clientPusher = new ClientPusher(`${process.env.PUSHER_KEY}`, {
  cluster: "eu",
  forceTLS: true,
 });
